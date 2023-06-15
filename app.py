e078b8bb6ead313cffe5b1eccbe01a3de00b7fab
João Morossini <joaomorossini@duck.com>
2023-06-15 15:55:25 -0300

    login is working

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import openai
 from dotenv import load_dotenv, find_dotenv
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
@@ -26,6 +26,16 @@ app.config['SECRET_KEY'] = 'thisisasecretkey'
 db = SQLAlchemy(app)  # Creating database
 bcrypt = Bcrypt(app)
 
+
+# ---------- LOG IN MANAGER ---------- #
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 # ---------- CREATING CLASSES AND FORMS ---------- #
 class User(db.Model, UserMixin):
@@ -90,12 +100,18 @@ def signup():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
     return render_template('login.html', form=form)
 
 
 @app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
-    return render_template('user_dashboard.html')
+    return render_template('dashboard.html')
 
 
 @app.route('/generator', methods=['GET', 'POST'])
