856328628cd7ea8f4653f75268edea4ae3f023db
João Morossini <joaomorossini@duck.com>
2023-06-18 14:23:35 -0300

    Password recovery implemented

--- app.py
+++ app.py
@@ -2,29 +2,42 @@
 # External dependencies
 import os
 import openai
+import jwt
+from time import time
 from dotenv import load_dotenv, find_dotenv
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, ValidationError
+from wtforms.validators import InputRequired, Length, ValidationError, Email, EqualTo
 from flask_bcrypt import Bcrypt
+from flask_mail import Mail, Message
 
 # Internal dependencies
 from prompt_template import prompt_template
 
-
 # ---------- FLASK APP AND DATABASE ---------- #
 app = Flask(__name__)  # Instantiating Flask class
+
 root_folder = os.path.dirname(os.path.abspath(__file__))
 database_path = os.path.join(root_folder, 'instance', 'database.db')
+
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{database_path}"  # Connecting app to the database
 # REFACTOR: SECRET KEY SHOULDN'T BE VISIBLE IN A PRODUCTION ENVIRONMENT
 app.config['SECRET_KEY'] = 'thisisasecretkey'
 db = SQLAlchemy(app)  # Creating database
+
 bcrypt = Bcrypt(app)
 
+# ---------- FLASK MAIL ---------- #
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'  # or your preferred SMTP server
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+
+mail = Mail(app)
 
 # ---------- LOG IN MANAGER ---------- #
 login_manager = LoginManager()
@@ -36,42 +49,82 @@ login_manager.login_view = 'login'
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+# ---------- SEND RESET EMAIL FUNCTION ---------- #
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request',
+                  sender='noreply@demo.com',
+                  recipients=[user.email])
+    msg.body = f'''To reset your password, visit the following link:
+{url_for('reset_token', token=token, _external=True)}
+If you did not make this request then simply ignore this email and no changes will be made.
+'''
+    mail.send(msg)
+
 # ---------- CREATING CLASSES AND FORMS ---------- #
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
+    email = db.Column(db.String(30), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
 
+    def get_reset_token(self, expires_sec=1800):
+        payload = {
+            'user_id': self.id,
+            'exp': time() + expires_sec
+        }
+        return jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_token(token):
+        try:
+            payload = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            user_id = payload['user_id']
+        except jwt.ExpiredSignatureError:
+            return None
+        except jwt.InvalidTokenError:
+            return None
+        return User.query.get(user_id)
 
 class SignupForm(FlaskForm):
-    username = StringField(validators=[
-                           InputRequired(), Length(min=4, max=30)], render_kw={"placeholder": "Username"})
+    email = StringField(validators=[
+                           InputRequired(), Email(), Length(min=4, max=30)], render_kw={"placeholder": "e-mail"})
 
     password = PasswordField(validators=[
                              InputRequired(), Length(min=4, max=40)], render_kw={"placeholder": "Password"})
 
     submit = SubmitField('Signup')
 
-    def validate_username(self, username):
-        existing_user_username = User.query.filter_by(
-            username=username.data).first()
-        if existing_user_username:
+    def validate_email(self, email):
+        existing_user_email = User.query.filter_by(
+            email=email.data).first()
+        if existing_user_email:
             raise ValidationError(
-                'That username already exists. Please choose a different one.')
+                'That user e-mail already exists. Please choose a different one.')
 
 
 class LoginForm(FlaskForm):
-    username = StringField(validators=[
-                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    email = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "E-mail"})
 
     password = PasswordField(validators=[
-                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+                             InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
 
     submit = SubmitField('Login')
 
 
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[InputRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
+
+
+
 # ---------- ROUTES ---------- #
-# Defining routes
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('index.html')
@@ -83,7 +136,7 @@ def signup():
 
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(username=form.username.data, password=hashed_password)
+        new_user = User(email=form.email.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('login'))
@@ -95,7 +148,7 @@ def signup():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
+        user = User.query.filter_by(email=form.email.data).first()
         if user:
             if bcrypt.check_password_hash(user.password, form.password.data):
                 login_user(user)
@@ -114,7 +167,6 @@ def dashboard():
     return render_template('dashboard.html', user=current_user)
 
 
-
 @app.route('/generator', methods=['GET', 'POST'])
 @login_required
 def generator():
@@ -131,6 +183,33 @@ def generator():
                                         additional_instructions=additional_instructions)
         response = get_completion(prompt)
     return render_template('generator.html', response=response)
+
+
+@app.route('/reset_request', methods=['GET', 'POST'])
+def reset_request():
+    form = RequestResetForm()
+    message = None
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_reset_email(user)
+            message = 'An e-mail has been sent with instructions to reset your password.'
+    return render_template('reset_request.html', form=form, message=message)
+
+
+@app.route('/reset_request/<token>', methods=['GET', 'POST'])
+def reset_token(token):
+    user = User.verify_reset_token(token)
+    if not user:
+        # If the token is invalid or expired, redirect the user to the `reset_request` route.
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        user.password = hashed_password
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('reset_token.html', form=form)
 
 
 # ---------- ENVIRONMENT VARIABLE API KEY ---------- #
@@ -158,12 +237,6 @@ with app.app_context():
         print("Tables created.")
     except Exception as e:
         print("An error occurred while creating tables:", e)
-
-
-print("Current working directory:", os.getcwd())
-
-
-print("SQLALCHEMY_DATABASE_URI:", app.config['SQLALCHEMY_DATABASE_URI'])
 
 
 # ---------- END ---------- #
