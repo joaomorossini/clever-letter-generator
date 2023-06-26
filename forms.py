11982cb6ee64213f6a28cf930892743ed2a41725
João Morossini <joaomorossini@duck.com>
2023-06-26 11:06:23 -0300

    created forms.py, models.py and routes.py

---
+++ forms.py
@@ -0,0 +1,38 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import InputRequired, Length, ValidationError, Email, EqualTo
+from models import User
+
+class SignupForm(FlaskForm):
+    email = StringField(validators=[
+                           InputRequired(), Email(), Length(min=4, max=50)], render_kw={"placeholder": "Email *"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=4, max=50)], render_kw={"placeholder": "Password *"})
+
+    submit = SubmitField('Signup')
+
+    def validate_email(self, email):
+        existing_user_email = User.query.filter_by(
+            email=email.data).first()
+        if existing_user_email:
+            flash('That e-mail account has already been used', 'warning')
+            raise ValidationError('That user e-mail already exists. Please choose a different one.')
+
+
+class LoginForm(FlaskForm):
+    email = StringField(validators=[InputRequired(), Length(min=4, max=50)], render_kw={"placeholder": "Email *"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=50)], render_kw={"placeholder": "Password *"})
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[InputRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
--- models.py
+++ models.py
@@ -7,7 +7,36 @@ from app import app
 db = SQLAlchemy(app)
 
 class User(db.Model, UserMixin):
-    # ... same as before ...
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(500), nullable=False)
+    api_key = db.Column(db.String(200))
+    cv = db.Column(db.String(5000), default='')
+
+    def get_reset_token(self, expires_sec=1800):
+        payload = {
+            'user_id': self.id,
+            'exp': time() + expires_sec
+        }
+        return jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_token(token):
+        try:
+            payload = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            user_id = payload['user_id']
+        except jwt.ExpiredSignatureError:
+            return None
+        except jwt.InvalidTokenError:
+            return None
+        return User.query.get(user_id)
 
 class Log(db.Model):
-    # ... same as before ...
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    job_title = db.Column(db.String(100), nullable=False)
+    employer_name = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Log('{self.timestamp}', '{self.employer_name}', '{self.job_title}')"
