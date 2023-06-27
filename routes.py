7a945dfd88539c0f221d4e4679c0b266f96bae3e
João Morossini <joaomorossini@duck.com>
2023-06-27 12:16:36 -0300

    logo image in base1.html now links to login and in base2.html links to generator

--- routes.py
+++ routes.py
@@ -250,6 +250,6 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        temperature=0,  # this is the degree of randomness of the model's output
+        temperature=0.5,  # this is the degree of randomness of the model's output
     )
     return response.choices[0].message["content"]
